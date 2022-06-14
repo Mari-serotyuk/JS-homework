cae0a5a4b0aae33046b64eba6868d6c19ea1e36d
Mari-serotyuk <99201320+Mari-serotyuk@users.noreply.github.com>
2022-06-14 18:41:14 +0300

    Add files via upload

--- Homework2/script/script.js
+++ Homework2/script/script.js
@@ -21,4 +21,27 @@ let shoppingList; //список покупок
 
 let userNetworkStatus; // является ли пользователь в сети
 
-let employeeSalary; // 
+let employeeSalary; // зарплата работника
+
+let cursorCoordinates //координаты курсора
+
+//задание 3
+
+let greeting;
+
+greeting = 'Hello world!'
+console.log(greeting);
+
+const num1 = 12;
+const num2 = 5;
+
+const result = num1 + num2;
+console.log(`result`, result);
+
+
+const userName2 = 'Ann';
+const space = ' ';
+const userDoing = 'runs';
+
+const fullSentence = userName2 + space + userDoing;
+console.log(`fullSentence`, fullSentence);

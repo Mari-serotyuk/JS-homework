1b45405e0b8ae6614c1fbe758ddbadb5926743e9
Mari-serotyuk <99201320+Mari-serotyuk@users.noreply.github.com>
2022-06-14 17:42:01 +0300

    Add files via upload

---
+++ Homework2/script/script.js
@@ -0,0 +1,24 @@
+//задание 1
+
+let userName;
+
+userName = 'Irina';
+
+let userSurname;
+
+userSurname = 'Ivanova'
+
+let userAge;
+
+userAge = 27;
+
+
+//задание 2
+
+let userNew; //новый пользователь
+
+let shoppingList; //список покупок
+
+let userNetworkStatus; // является ли пользователь в сети
+
+let employeeSalary; // 

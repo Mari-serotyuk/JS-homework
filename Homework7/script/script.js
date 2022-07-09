8479931145e40ddaafa1883213c5ee17a2940c74
Mari-serotyuk <99201320+Mari-serotyuk@users.noreply.github.com>
2022-07-09 03:53:04 +0300

    Add files via upload

--- Homework7/script/script.js
+++ Homework7/script/script.js
@@ -1,36 +1,36 @@
 //Задание 1
 
-const schedule = {};
+// const schedule = {};
 
-do{
-    const key = prompt('Введите время, используя цифры и знак \':\'');
-    if(key === null){
+// do{
+//     const key = prompt('Введите время, используя цифры и знак \':\'');
+//     if(key === null){
 
-        break;
+//         break;
 
-    }else {
-        const value = prompt('Введите задачу, которую нужно запланировать'); 
-        if(value === null){
-            
-            break;
+//     }else {
+//         const value = prompt('Введите задачу, которую нужно запланировать'); 
+//         if(value === null){
 
-        }else {
-            schedule[key] = value;
-        }
-    } 
+//             break;
 
-}while(true);
+//         }else {
+//             schedule[key] = value;
+//         }
+//     } 
 
-console.log(`schedule`, schedule)
+// }while(true);
+
+// console.log(`schedule`, schedule)
 
 
 
 //Задание 2
 
 const salaries = {
-    Ann: '500',
-    Bob: '400',
-    Sam: '450'
+    Ann: '500.50',
+    Bob: '400.70',
+    Sam: '450.30'
 };
 
 let sum = 0;
@@ -38,4 +38,4 @@ for (let key in salaries){
     sum += Number(salaries[key]);
 }
 
-// alert(sum);
+console.log(`sum.toFixed(2)`, sum.toFixed(2));

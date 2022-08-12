eb55a049cc1c5d53e2139add703862b370932f50
Mari-serotyuk <99201320+Mari-serotyuk@users.noreply.github.com>
2022-08-12 15:03:48 +0300

    Add files via upload

--- Homework13/script/script.js
+++ Homework13/script/script.js
@@ -1,71 +1,71 @@
 //Первый способ
 
-// const slides = document.querySelectorAll('#slides .slide');
-// const currentSlide = 0;
+const slides = [...document.querySelectorAll('#slides .slide')];
+let currentSlide = 0;
 
 
-// function goToSlide(n) {
-//     slides[currentSlide].className = 'slide';
-//     currentSlide = (n + slides.length)%slides.length;
-//     slides[currentSlide].className = 'slide showing';
-// }
+function goToSlide(n) {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (n + slides.length)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
 
-// function nextSlide() {
-//     goToSlide(currentSlide + 1)
-// }
+function nextSlide() {
+    goToSlide(currentSlide + 1)
+}
 
-// function previousSlide() {
-//     goToSlide(currentSlide - 1)
+function previousSlide() {
+    goToSlide(currentSlide - 1)
     
-// }
+}
 
-// const next = document.getElementById('next');
-// const previous = document.getElementById('previous');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
 
-// next.onclick = function(){
-//     nextSlide();
-// };
+next.onclick = function(){
+    nextSlide();
+};
 
-// previous.onclick = function(){
-//     previousSlide();
-// };
+previous.onclick = function(){
+    previousSlide();
+};
 
 
 // Второй способ
 
-const slides = document.querySelectorAll('.slide');
-const next = document.getElementById('next');
-const previous = document.getElementById('previous');
+// const slides = [...document.querySelectorAll('.slide')];
+// const next = document.getElementById('next');
+// const previous = document.getElementById('previous');
 
-const currentSlide = 0;
+// let currentSlide = 0;
 
-const nextSlide = () => {
+// const nextSlide = () => {
 
-    slides[currentSlide].classList.add('hidden')
+//     slides[currentSlide].classList.add('hidden')
 
-    if(currentSlide === slides.length - 1){
+//     if(currentSlide === slides.length - 1){
 
-        slides[0].classList.remove('hidden')
-        currentSlide = 0
-    } else {
-        slides[currentSlide + 1].classList.remove['hidden']
-        currentSlide++
-    }
-}
+//         slides[0].classList.remove('hidden')
+//         currentSlide = 0
+//     } else {
+//         slides[currentSlide + 1].classList.remove('hidden')
+//         currentSlide++
+//     }
+// }
 
-const previousSlide = () => {
+// const previousSlide = () => {
 
-    slides[currentSlide].classList.add('hidden')
+//     slides[currentSlide].classList.add('hidden')
     
-    if(currentSlide === 0){
-        slides[slides.length - 1].classList.remove('hidden')
-        currentSlide = slides.length - 1
-    }else{
-        slides[currentSlide - 1].classList.remove('hidden')
-        currentSlide--
-    }
+//     if(currentSlide === 0){
+//         slides[slides.length - 1].classList.remove('hidden')
+//         currentSlide = slides.length - 1
+//     }else{
+//         slides[currentSlide - 1].classList.remove('hidden')
+//         currentSlide--
+//     }
     
-}
+// }
 
-next.addEventListener('click', nextSlide);
-previous.addEventListener('click', previousSlide);
+// next.addEventListener('click', nextSlide);
+// previous.addEventListener('click', previousSlide);
